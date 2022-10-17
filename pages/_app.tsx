eab1a68f581a76ed04487ba4ee64fe64a17c32a1
Zainab Oufqir <zayna.oufkir@gmail.com>
2022-10-17 22:09:48 +0100

    add navbar and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import Script from "next/script";
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from "next-themes";
+import { Navbar, Footer } from '../components';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider attribute='class'>
+    <div className='dark:bg-nft-dark bg-white min-h-screen'>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/36c479c05f.js" crossorigin="anonymous" />
+  </ThemeProvider>
+ 
+  
 }
 
 export default MyApp
