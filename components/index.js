eef1cd9b3379a615e098b5f5122b5d1ba1d6fe63
Zainab Oufqir <zayna.oufkir@gmail.com>
2022-10-17 23:09:44 +0100

    add creators section

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Banner from './Banner';
+import CreatorCard from './CreatorCard';
 
-export { Navbar, Footer, Banner };
+export { Navbar, Footer, Banner, CreatorCard };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,90 @@
 import type { NextPage } from 'next'
-import { Banner } from '../components'
+import {useRef, useState, useEffect} from 'react'
+import { useTheme } from 'next-themes';
+import { Banner, CreatorCard } from '../components'
+import images from '../assets';
+import Image from 'next/image';
+import { makeid } from '../utils/makeId';
 
 const Home: NextPage = () => {
+  const [hideButtons, setHideButtons] = useState(false);
+
+  const parentRef = useRef(null);
+  const scrollRef = useRef(null);
+
+  const { theme } = useTheme();
+
+  const handleScroll = (direction: any) => {
+    const { current } = scrollRef;
+
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (direction === 'left') {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+    // check if scrollRef container is overfilling its parentRef container
+    const isScrollable = () => {
+      const { current } = scrollRef;
+      const { current: parent } = parentRef;
+  
+      if (current?.scrollWidth >= parent?.offsetWidth) return setHideButtons(false);
+      return setHideButtons(true);
+    };
+  
+    // if window is resized
+    useEffect(() => {
+      isScrollable();
+      window.addEventListener('resize', isScrollable);
+  
+      return () => {
+        window.removeEventListener('resize', isScrollable);
+      };
+    });
+
   return (
     <div className="flex justify-center sm:px-4 p-12">
-    <div className="w-full minmd:w-4/5">
-      <Banner
-        name={(<>Discover, collect, and sell <br /> extraordinary NFTs</>)}
-        childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
-        parentStyle="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
-      />
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          name={(<>Discover, collect, and sell <br /> extraordinary NFTs</>)}
+          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+          parentStyle="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        />
+
+        <div>
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
+            Best Creators
+          </h1>
+
+          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+                <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
+                  {[6, 7, 8, 9, 10].map((i) => (
+                      <CreatorCard
+                        key={`creator-${i}`}
+                        rank={i}
+                        creatorImage={images[`creator${i}`]}
+                        creatorName={`0x${makeid(3)}...${makeid(4)}`}
+                        creatorEths={10 - i * 0.534}
+                      />
+                    ))}
+                    {!hideButtons && (
+                    <>
+                      <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
+                        <Image src={images.left} layout="fill" objectFit="contain" alt="left_arrow" className={theme === 'light' ? 'filter invert' : undefined} />
+                      </div>
+                      <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
+                        <Image src={images.right} layout="fill" objectFit="contain" alt="left_arrow" className={theme === 'light' ? 'filter invert' : undefined} />
+                      </div>
+                    </>
+                  )}
+                </div>
+          </div>
+        </div>
+
+
       </div>
     </div>
   )
