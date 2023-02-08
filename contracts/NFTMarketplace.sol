3f42d6b617387bc6329ea4282c65be4acbe67464
Zainab Oufqir <zayna.oufkir@gmail.com>
2023-02-08 21:54:34 +0100

    add create,list and buy token functions to smartcontract

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -14,15 +14,24 @@ contract NFTMarketplace is ERC721URIStorage, Ownable {
     uint256 listPrice = 0.0001 ether;
     address payable owner;
 
-    struct ListedToken {
+    struct TokenList {
         uint256 tokenId;
-        address payable owner;
-        address payable seller;
+        address owner;
+        address seller;
         uint256 price;
-        bool listed;
+        bool currentlyListed;
     }
 
-    mapping(uint256 => ListedToken) idToListenTokens;
+    mapping(uint256 => TokenList) tokenLists;
+    mapping(address => uint256) tokenBalance;
+    mapping(address => uint256) funds;
+    event TokenListed(
+        uint256 indexed tokenId,
+        address owner,
+        address seller,
+        uint256 price,
+        bool currentlyListed
+    );
 
     constructor() ERC721("NFTMarketplace", "MKP") {
         owner = payable(msg.sender);
@@ -30,7 +39,7 @@ contract NFTMarketplace is ERC721URIStorage, Ownable {
 
     modifier ListedTokenExist(uint256 tokenId) {
         require(
-            idToListenTokens[tokenId].owner != address(0),
+            tokenLists[tokenId].owner != address(0),
             "This token is not listed"
         );
         _;
@@ -44,20 +53,102 @@ contract NFTMarketplace is ERC721URIStorage, Ownable {
         return listPrice;
     }
 
-    function getLatestListedToken() public view returns (ListedToken memory) {
-        return idToListenTokens[_tokenIds.current()];
+    function getLatestListedToken() public view returns (TokenList memory) {
+        return tokenLists[_tokenIds.current()];
     }
 
     function getListedToken(uint256 tokenId)
         public
         view
         ListedTokenExist(tokenId)
-        returns (ListedToken memory)
+        returns (TokenList memory)
     {
-        return idToListenTokens[tokenId];
+        return tokenLists[tokenId];
     }
 
     function getCurrentTokenId() public view returns (uint256) {
         return _tokenIds.current();
+    }
+
+    function createToken(string memory _tokenURI, uint256 price)
+        external
+        payable
+        returns (uint256)
+    {
+        require(msg.value == listPrice, "invalid balance for listing");
+        require(price > 0, "price should be grather than 0");
+        _tokenIds.increment();
+        uint256 id = _tokenIds.current();
+        _safeMint(msg.sender, id);
+        _setTokenURI(id, _tokenURI);
+        tokenBalance[msg.sender]++;
+        createTokenList(id, price);
+        return id;
+    }
+
+    function createTokenList(uint256 tokenId, uint256 price) private {
+        tokenLists[tokenId] = TokenList(
+            tokenId,
+            address(this),
+            msg.sender,
+            price,
+            true
+        );
+        _transfer(msg.sender, address(this), tokenId);
+    }
+
+    function getAllNFTs() external view returns (TokenList[] memory) {
+        uint256 allTokens = _tokenIds.current();
+        uint256 numberOfTokensListed = allTokens - _itemsSold.current();
+        TokenList[] memory tokens = new TokenList[](numberOfTokensListed);
+        uint256 index;
+        for (uint256 i = 1; i < allTokens; i++) {
+            if (tokenLists[i].currentlyListed) {
+                tokens[index] = tokenLists[i];
+                index++;
+            }
+        }
+
+        return tokens;
+    }
+
+    function getMyNFTs() external view returns (TokenList[] memory) {
+        uint256 numberOfTokens = _tokenIds.current();
+        uint256 userTokens = tokenBalance[msg.sender];
+        TokenList[] memory tokens = new TokenList[](userTokens);
+        uint256 index;
+        for (uint256 i = 1; i <= numberOfTokens; i++) {
+            if (tokenLists[i].seller == msg.sender) {
+                tokens[index] = tokenLists[i];
+                index++;
+            }
+        }
+        return tokens;
+    }
+
+    function executeSale(uint256 tokenId) external payable {
+        TokenList storage token = tokenLists[tokenId];
+        require(
+            token.currentlyListed && token.seller != address(0),
+            "token does not exist"
+        );
+        require(msg.value == token.price, "invalid amount");
+        _itemsSold.increment();
+        tokenBalance[msg.sender]--;
+        _transfer(address(this), msg.sender, tokenId);
+        approve(address(this), tokenId);
+        funds[owner] += listPrice;
+        funds[token.seller] += token.price;
+        token.currentlyListed = false;
+        token.owner = payable(msg.sender);
+        token.seller = payable(address(0));
+    }
+
+    function withdraw() external {
+        uint256 balance = funds[msg.sender];
+        require(balance > 0, "unsufficent balance");
+        funds[msg.sender] = 0;
+        (bool sent, ) = payable(msg.sender).call{value: balance}("");
+        require(sent, "transfer failed");
     }
 }
