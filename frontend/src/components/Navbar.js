3f1710e4b4db1f9723a19e4f75e6643ff1649ffa
Zainab Oufqir <zayna.oufkir@gmail.com>
2023-02-09 03:39:11 +0100

    frontend: connect wallet, create and list nft

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,11 @@
-import { Switch, Route, Link, useRouteMatch, useParams } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 
@@ -6,6 +13,52 @@ function Navbar() {
   const [connected, toggleConnect] = useState(false);
   const location = useLocation();
   const [currAddress, updateAddress] = useState("0x");
+  const ethers = require("ethers");
+
+  async function getAddress() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const addr = await signer.getAddress();
+    updateAddress(addr);
+  }
+
+  function updateButton() {
+    const ethereumButton = document.querySelector(".enableEthereumButton");
+    ethereumButton.textContent = "Connected";
+    ethereumButton.classList.remove("hover:bg-blue-70");
+    ethereumButton.classList.remove("bg-blue-500");
+    ethereumButton.classList.add("hover:bg-green-70");
+    ethereumButton.classList.add("bg-green-500");
+  }
+
+  async function connectWebsite() {
+    const chainId = await window.ethereum.request({ method: "eth_chainId" });
+    if (chainId !== "0x5") {
+      //alert('Incorrect network! Switch your metamask network to goerli');
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x5" }],
+      });
+    }
+    await window.ethereum.request({ method: "eth_requestAccounts" }).then(() => {
+      updateButton();
+      getAddress();
+      // window.location.replace(location.pathname);
+    });
+  }
+
+  useEffect(() => {
+    let val = window.ethereum.isConnected();
+    if (val) {
+      getAddress();
+      toggleConnect(val);
+      updateButton();
+    }
+
+    window.ethereum.on("accountsChanged", function (accounts) {
+      window.location.replace(location.pathname);
+    });
+  });
 
   return (
     <div className="">
@@ -16,7 +69,7 @@ function Navbar() {
               <div className="inline-block font-bold text-xl ml-2">NFT Marketplace</div>
             </Link>
           </li>
-          <li className="w-3/6">
+          <li className="w-2/6">
             <ul className="lg:flex justify-between font-bold mr-10 text-lg">
               {location.pathname === "/" ? (
                 <li className="border-b-2 hover:pb-0 p-2">
@@ -46,7 +99,10 @@ function Navbar() {
                 </li>
               )}
               <li>
-                <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm">
+                <button
+                  className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm"
+                  onClick={connectWebsite}
+                >
                   {connected ? "Connected" : "Connect Wallet"}
                 </button>
               </li>
--- frontend/src/components/SellNFT.js
+++ frontend/src/components/SellNFT.js
@@ -1,21 +1,75 @@
 import Navbar from "./Navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { uploadFileToIPFS, uploadJSONToIPFS } from "../pinata";
 import Marketplace from "../Marketplace.json";
 import { useLocation } from "react-router";
+import { redirect } from "react-router-dom";
 
 export default function SellNFT() {
   const [formParams, updateFormParams] = useState({ name: "", description: "", price: "" });
   const [fileURL, setFileURL] = useState(null);
-  const ethers = require("ethers");
   const [message, updateMessage] = useState("");
   const location = useLocation();
+  const ethers = require("ethers");
+
+  const onChangeFile = async (e) => {
+    const file = e.target.files[0];
+    console.log({ file });
+    try {
+      const response = await uploadFileToIPFS(file);
+      if (response.success) {
+        console.log(`file uploaded to ${response.pinataURL}`);
+        setFileURL(response.pinataURL);
+      }
+    } catch (error) {
+      console.log("error while upload image ", error);
+    }
+  };
+
+  const uploadMetadataToIPFS = async () => {
+    const { name, description, price } = formParams;
+    if (!name || !description || !price || !fileURL) return;
+    const jsonData = { name, description, price, image: fileURL };
+    try {
+      const { success, pinataURL } = await uploadJSONToIPFS(jsonData);
+      if (success) {
+        console.log(`Json uploaded to ${pinataURL}`);
+        return pinataURL;
+      }
+    } catch (error) {
+      console.log("error while upload json metadata ", error);
+    }
+  };
+
+  const listNFT = async (e) => {
+    e.preventDefault();
+    try {
+      const metadataURL = await uploadMetadataToIPFS();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      updateMessage("Please wait while uploading ...");
+
+      let contract = new ethers.Contract(Marketplace.address, Marketplace.abi, signer);
+      const price = ethers.utils.parseEther(formParams.price);
+      const listingPrice = (await contract.getListPrice()).toString();
+      console.log({ listingPrice });
+      const transation = await contract.createToken(metadataURL, price, { value: listingPrice });
+      await transation.wait();
+      alert("Successfuly listed your nft");
+      updateMessage("");
+      updateFormParams({ name: "", description: "", price: "" });
+      redirect("/");
+    } catch (error) {
+      console.log("error while list NFT ", error);
+    }
+  };
 
   return (
     <div className="">
       <Navbar></Navbar>
       <div className="flex flex-col place-items-center mt-10" id="nftForm">
-        <form className="bg-white shadow-md rounded px-8 pt-4 pb-8 mb-4">
+        <form onSubmit={listNFT} className="bg-white shadow-md rounded px-8 pt-4 pb-8 mb-4">
           <h3 className="text-center font-bold text-purple-500 mb-8">
             Upload your NFT to the marketplace
           </h3>
@@ -64,7 +118,7 @@ export default function SellNFT() {
             <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="image">
               Upload Image
             </label>
-            <input type={"file"} onChange={""}></input>
+            <input type={"file"} onChange={onChangeFile}></input>
           </div>
           <br></br>
           <div className="text-green text-center">{message}</div>
